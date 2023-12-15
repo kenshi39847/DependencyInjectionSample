07583515777133ff2b73336a97fe52ae49d19d12
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-15 10:27:54 +0900

    Before Adviceの作成

--- src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -1,10 +1,22 @@
 package com.example.demo.chapter03.aop;
 
+import java.text.SimpleDateFormat;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class SampleAspect {
-
+	@Before("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+	public void beforeAdvice(JoinPoint joinpoint) {
+		//分かりやすいように表示
+		System.out.println("=====Before Advice=====");
+		//日付表示
+		System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
+		//メソッド名を表示
+		System.out.println(String.format("メソッド:%s", joinpoint.getSignature().getName()));
+	}
 }
